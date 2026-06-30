f4c9153562ebef8e19b854e5484a25faa0c9bac5
Nayanvavadiya <nayanvavdiya062@gmail.com>
2026-06-30 11:29:33 +0530

    Fix signup and mailer issues

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -7,6 +8,29 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS,
     },
 });
+
+transporter.verify((err, success) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("SMTP Connected!");
+    }
+});
+
+
+
+// const nodemailer = require("nodemailer");
+
+// // console.log("EMAIL_USER:", process.env.EMAIL_USER);
+// // console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
 
 
 
